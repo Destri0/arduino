a4924f36d8673e2cfb0e40b48b77f867f8c3f735
Destri0 <109970051+Destri0@users.noreply.github.com>
2023-10-30 08:36:23 +0100

    Add files via upload

---
+++ Arduino UNO/led_pulsador_encendido_apagado 2.0/led_pulsador_encendido_apagado.ino
@@ -0,0 +1,25 @@
+const int pul = 7;  
+const int led1 = 9;   
+
+int estado = 0;     
+int uestado = 0;  
+int brillo = LOW; 
+
+void setup() {
+  pinMode(pul, INPUT);
+  pinMode(led1, OUTPUT);
+}
+
+void loop() {
+  estado = digitalRead(pul);
+  if (estado != uestado) {
+    if (estado == HIGH) {
+      while (digitalRead(pul) == HIGH) {
+      }
+      brillo = (brillo == LOW) ? HIGH : LOW;
+      digitalWrite(led1, brillo);
+    }
+    delay(50); 
+  }
+  uestado = estado;
+}
