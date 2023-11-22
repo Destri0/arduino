c72dc0f7abd6745197be5315b7b68d34f279b316
Fran <109970051+Destri0@users.noreply.github.com>
2023-11-22 20:55:03 +0100

    Add files via upload

---
+++ Arduino UNO/led_consola/led_consola.ino
@@ -0,0 +1,18 @@
+const int led1 = 9;
+
+void setup()
+{
+	pinMode(led1, OUTPUT);
+	Serial.begin(9600);
+	Serial.println("Introduzca un numero de 0 a 255:\n");
+}
+
+void loop()
+{
+	if( Serial.available() > 0 )
+	{       
+		delay(5);
+		analogWrite(led1, Serial.parseInt());
+        while(Serial.read() > -1 );
+	}
+}
